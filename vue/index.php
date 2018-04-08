4a02e2c13c778c2d62880237dd5afa8e7c702f3a
mukulrathi <mukulrathi8@gmail.com>
2018-04-08 12:09:16 +0000

    vue

--- vue/index.php
+++ vue/index.php
@@ -1,3 +1,11 @@
-<?php
-echo 'hello world for vue js '
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>vue js</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css" />
+<body>
+<h2>vue js applications</h2> 
+</body>
+</html>
