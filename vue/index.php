541f187fac9c1f9dca401e720cbde115af1d1341
mukulrathi <mukulrathi8@gmail.com>
2018-04-09 05:42:06 +0000

    bj

--- vue/index.php
+++ vue/index.php
@@ -116,7 +116,7 @@
 
 <!-- Footer -->
 <footer class="container-fluid bg-4 text-center">
-  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
+  <p>Bootstrap Theme Made By Bharat Jagtap.com</a></p> 
 </footer>
 
 </body>
